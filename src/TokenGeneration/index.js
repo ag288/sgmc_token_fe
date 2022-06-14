2773de65a40c4f32fff0ce11f407cc91717807a4
ag288 <athirageorge288@gmail.com>
2022-06-14 09:35:42 +0530

    changes

--- src/TokenGeneration/index.js
+++ src/TokenGeneration/index.js
@@ -146,7 +146,7 @@ export const TokenGeneration = () => {
                                             <PinInputField onChange={(e) => { setOtp(prev => prev + e.target.value) }} />
                                         </PinInput>
                                     </HStack>
-                                    <Timer time="40" setState={setState}/>
+                                    <Timer time="120" setState={setState}/>
                                     <Link align="right" fontSize={14} color={'blue.400'}>Resend OTP</Link>
                                     <Button
                                         type='submit'
