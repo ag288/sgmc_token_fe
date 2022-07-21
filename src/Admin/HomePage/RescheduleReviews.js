4bbfa79b6f3c0a4849e210f0a5a025460251258e
ag288 <athirageorge288@gmail.com>
2022-07-21 11:24:38 +0530

    phone view

--- src/Admin/HomePage/RescheduleReviews.js
+++ src/Admin/HomePage/RescheduleReviews.js
@@ -25,7 +25,7 @@ export const RescheduleReviews = () => {
             <Box
                 borderWidth="1px"
                 borderRadius="lg"
-                width={"full"}
+                width={"auto"}
                 bg={'white'}
                 mx="auto"
                 boxShadow={'2xl'}
@@ -38,7 +38,6 @@ export const RescheduleReviews = () => {
                                 <Tr>
                                     <Th>Name</Th>
                                     <Th>Phone</Th>
-                                    <Th>Date</Th>
                                     <Th>Token</Th>
                                     <Th>Type</Th>
                                     <Th>Est. Time</Th>
@@ -50,7 +49,6 @@ export const RescheduleReviews = () => {
                                         <Td >{item.name}</Td>
                                         <Td ><Text href={`tel:+${item.phone}`} as="a" bg="transparent" >{item.phone.substring(2)}</Text>
                                         </Td>
-                                        <Td >{new Date(item.date).toDateString()}</Td>
                                         <Td >{item.tokenNumber}</Td>
                                         <Td>{item.type}</Td>
                                         <Td>{new Date(`1970-01-01 ${item.timeInEst}`).toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata', hour: 'numeric', minute: "numeric" })}</Td>
--- src/ReviewBooking/ReviewList.js
+++ src/ReviewBooking/ReviewList.js
@@ -56,7 +56,7 @@ export const ReviewList = () => {
             setIsLoading(true)
             api.review.deleteReview({ id: item.reviewID }).then((res) => {
                 setIsLoading(false)
-               // window.location.reload()
+                // window.location.reload()
                 toast({
                     title: 'Deleted review successfully',
                     status: 'success',
@@ -82,67 +82,78 @@ export const ReviewList = () => {
     return (
         <Flex bg="gray.100"
             minH={"100vh"}>
-            <IconButton size="lg" onClick={() => navigate("/book-review")} icon={<ArrowBackIcon />}></IconButton>
             {isLoading ? <FullPageSpinner /> :
-                <Stack mx="auto" py={12}>
-                    <Heading size="md">Booked Tokens</Heading>
-                 {isLaptop &&   <Box
-                        rounded={'lg'}
-                        bg={'white'}
-                        boxShadow={'lg'}
-                        p={3}
-                        width='auto'>
-                        <Table variant='striped' colorScheme='grey'>
-                            <Thead>
-                                <Tr>
-                                    <Th>Name</Th>
-                                    <Th>Phone</Th>
-                                    <Th>Date</Th>
-                                    <Th>Token</Th>
-                                    <Th>Type</Th>
-                                    <Th>Est. Time</Th>
-                                    <Th></Th>
-                                </Tr>
-                            </Thead>
-                            <Tbody>
-                                {reviewlist.map((item, index) =>
-                                    <Tr key={index}>
-                                        <Td >{item.name}</Td>
-                                        <Td ><Text href={`tel:+${item.phone}`} as="a" bg="transparent" >{item.phone.substring(2)}</Text>
-                                        </Td>
-                                        <Td >{new Date(item.date).toDateString()}</Td>
-                                        <Td >{item.tokenNumber}</Td>
-                                        <Td>{item.type}</Td>
-                                        <Td>{new Date(`1970-01-01 ${item.timeInEst}`).toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata', hour: 'numeric', minute: "numeric" })}</Td>
-                                        <Td><IconButton bg="transparent" onClick={() => deleteReview(item)} icon={<DeleteIcon />}></IconButton></Td>
-                                    </Tr>
-                                )
-                                }
-                            </Tbody>
-                        </Table>
-                    </Box>}
+                <>
+                    {isLaptop && <>
 
-                    {isMobile && reviewlist.map((review)=><Box
-                        rounded={'lg'}
-                        bg={'white'}
-                        boxShadow={'lg'}
-                        p={3}
-                        width='full'>
-                            <VStack spacing={2} width="full" alignItems={"baseline"}>
-                       <HStack width="full" spacing={"auto"}><Heading size={"md"}>{review.name}</Heading>
-                       <IconButton bg="transparent" onClick={() => deleteReview(review)} icon={<DeleteIcon />}></IconButton>
-                       </HStack> 
-                       <Heading size="sm">{`${review.tokenNumber} (${review.type})`}</Heading>
-                        <Text>{`${new Date(review.date).toDateString()}, ${new Date(`1970-01-01 ${review.timeInEst}`).toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata', hour: 'numeric', minute: "numeric" })}`
-                        }</Text>
-                        <HStack>
-                        <Text >{review.phone.substring(2)}</Text>
-                       <IconButton icon={<FaPhoneAlt/>} href={`tel:+${review.phone}`} as="a" bg="transparent"></IconButton>
-                        </HStack>            
-                        </VStack>
-                        </Box>)}
-                </Stack>
-            }
+                        <IconButton size="lg" onClick={() => navigate("/book-review")} icon={<ArrowBackIcon />}></IconButton>
+
+                        <Stack py={12} px={2} mx="auto" width="auto">
+
+                            <Heading size="md">Booked Tokens</Heading>
+                            <Box
+                                rounded={'lg'}
+                                bg={'white'}
+                                boxShadow={'lg'}
+                                p={3}
+                                width='auto'>
+                                <Table variant='striped' colorScheme='grey'>
+                                    <Thead>
+                                        <Tr>
+                                            <Th>Name</Th>
+                                            <Th>Phone</Th>
+                                            <Th>Date</Th>
+                                            <Th>Token</Th>
+                                            <Th>Type</Th>
+                                            <Th>Est. Time</Th>
+                                            <Th></Th>
+                                        </Tr>
+                                    </Thead>
+                                    <Tbody>
+                                        {reviewlist.map((item, index) =>
+                                            <Tr key={index}>
+                                                <Td >{item.name}</Td>
+                                                <Td ><Text href={`tel:+${item.phone}`} as="a" bg="transparent" >{item.phone.substring(2)}</Text>
+                                                </Td>
+                                                <Td >{new Date(item.date).toDateString()}</Td>
+                                                <Td >{item.tokenNumber}</Td>
+                                                <Td>{item.type}</Td>
+                                                <Td>{new Date(`1970-01-01 ${item.timeInEst}`).toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata', hour: 'numeric', minute: "numeric" })}</Td>
+                                                <Td><IconButton bg="transparent" onClick={() => deleteReview(item)} icon={<DeleteIcon />}></IconButton></Td>
+                                            </Tr>
+                                        )
+                                        }
+                                    </Tbody>
+                                </Table>
+                            </Box>
+                        </Stack>
+                    </>}
+                    {isMobile &&
+                        <Stack width="full" alignItems="baseline" py={2} mx="2">
+                            <IconButton size="lg" onClick={() => navigate("/book-review")} icon={<ArrowBackIcon />}></IconButton>
+                            <Heading size="md">Booked Tokens</Heading>
+                            {reviewlist.map((review) => <Box
+                                rounded={'lg'}
+                                bg={'white'}
+                                boxShadow={'lg'}
+                                p={3}
+                                width='full'>
+                                <VStack spacing={2} width="full" alignItems={"baseline"}>
+                                    <HStack width="full" spacing="auto">
+                                        <HStack ><Heading size={"md"}>{review.name}</Heading>
+                                            <IconButton icon={<FaPhoneAlt />} href={`tel:+${review.phone}`} as="a" bg="transparent"></IconButton>
+
+                                        </HStack>
+                                        <IconButton bg="transparent" onClick={() => deleteReview(review)} icon={<DeleteIcon />}></IconButton>
+                                    </HStack>
+                                    <Heading size="sm">{`${review.tokenNumber} (${review.type}), ${new Date(`1970-01-01 ${review.timeInEst}`).toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata', hour: 'numeric', minute: "numeric" })}`}</Heading>
+
+                                </VStack>
+                            </Box>)}
+                        </Stack>
+                    }
+
+                </>}
         </Flex>
     )
 }
